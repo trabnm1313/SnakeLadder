68e5ff39a7943c43e542ca64dc8c86c07ccaa81c
trabnm1313 <56515496+trabnm1313@users.noreply.github.com>
2020-04-10 23:18:26 +0700

    Game now playable?
    - Coding basic system of how game work, now playable
    - Update game map
    - fix bugs < a lot >

--- Button.c
+++ Button.c
@@ -25,6 +25,19 @@ int IsClicked(bool mouseOn){
     return btnState;
 }
 
+void delay(int number_of_seconds) 
+{ 
+    // Converting time into milli_seconds 
+    int milli_seconds = 1000 * number_of_seconds; 
+  
+    // Storing start time 
+    clock_t start_time = clock(); 
+  
+    // looping till required time is not achieved 
+    while (clock() < start_time + milli_seconds) 
+        ; 
+} 
+
 //Check if button is over given buttonRec or not return bool
 bool IsMouseOver(Vector2 mousePos, Rectangle srcRec){
     bool mouseOn;
@@ -45,6 +58,7 @@ int main(void)
     Image btnImg = LoadImage("src/img/btn.png");
     
     Texture2D rollBtn = LoadTexture("src/img/roll.png");
+    Texture2D battleBtn = LoadTexture("src/img/roll.png");
     Texture2D menuBg = LoadTexture("src/img/menu.png");
     Texture2D backGround = LoadTexture("src/img/bg.png");
     Texture2D startBtn = LoadTextureFromImage(btnImg);
@@ -57,20 +71,24 @@ int main(void)
     Rectangle greenRec = {323, 620, 87, 70};
     Rectangle startBtnRec = {0, 0, startBtn.width, startBtn.height};
     Rectangle rollBtnRec = {0, 0, rollBtn.width, rollFrameHeight};
+    Rectangle battleBtnRec = {0, 0, battleBtn.width, rollFrameHeight};
     Rectangle Char1 = {0, 0, 50, 50};
     
     Rectangle startBtnBound = {screenWidth / 2 - startBtn.width / 2, screenHeight / 4, startBtn.width, startBtn.height};
     Rectangle rollBtnBound = {1030, 570, rollBtn.width, rollFrameHeight};
+    Rectangle battleBtnBound = {1025, 25, battleBtn.width, rollFrameHeight};
     
-    bool gameStart = false;
+    bool gameStart = false, battleBegin = false, playerRoll = false, battleDone = false, delayWin = false;
     bool mouseOn1, mouseOn2, mouseOn3, mouseOn4;
     
     int btnState = 0;
-    int randNum;
+    int randNum, randNumBattle;
     int playerPos[4];
-    int playerPlay, playerTurn;
+    int playerPlay, playerTurn=0, loser;
+    int samePos = 0, player1Randnum, player2Randnum, clickBattle=0;
+    int framesCounter = 0, currentFrame = 0;
     
-    char randStr[3] = "";
+    char randStr[3] = "", player1Randstr[3] = "", player2Randstr[3] = "", playerPosStr[4][3];
     
     SetTargetFPS(60);
     playerPos[0] = 0;
@@ -85,12 +103,28 @@ int main(void)
         Vector2 mousePos = GetMousePosition();
         mouseOn1 = IsMouseOver(mousePos, startBtnBound);
         mouseOn2 = IsMouseOver(mousePos, rollBtnBound);
-        
-        playerTurn = 0;
+        mouseOn3 = IsMouseOver(mousePos, battleBtnBound);
         
         if(IsKeyPressed(KEY_F11)){
             ToggleFullscreen();
             SetWindowPosition(50, 50);
+        }
+        
+        if(playerTurn > 3){
+            playerTurn = 0;
+        }
+        
+         if (framesCounter >= (60/8)){
+            framesCounter = 0;
+            currentFrame++;
+
+            if (currentFrame > 5){
+                currentFrame = 0;
+                battleBegin = false;
+                playerPos[playerTurn] -= 1;
+                clickBattle = 0;
+            }
+            
         }
         
         // Draw
@@ -98,17 +132,102 @@ int main(void)
             
             ClearBackground(RAYWHITE);
             if(gameStart == true){
+                
                 DrawTexture(backGround, 0, 0, WHITE);
-                DrawTextureRec(rollBtn, rollBtnRec, (Vector2){rollBtnBound.x, rollBtnBound.y}, WHITE);
-                if(IsClicked(mouseOn2) == 1){
-                   rollBtnRec.y = 0;
-                   randNum = GetRandomValue(1, 6);
-                   playerPos[playerTurn] = playerPos[playerTurn] + randNum;
-                   sprintf(randStr, "%d", randNum);
-                }else if(IsClicked(mouseOn2) == 2) rollBtnRec.y = rollFrameHeight;
+                DrawText(randStr, 1130, 500, 50, BLACK);
+                if(battleBegin == true){
+                    DrawTextureRec(battleBtn, battleBtnRec, (Vector2){battleBtnBound.x, battleBtnBound.y}, WHITE);
+                    if(IsClicked(mouseOn3) == 1){
+                        battleBtnRec.y = 0;
+                        if(clickBattle == 0){
+                            randNumBattle = GetRandomValue(1, 6);
+                            player1Randnum = randNumBattle;
+                            sprintf(player1Randstr, "%d", randNumBattle);
+                            clickBattle = 1;
+                            randNumBattle = GetRandomValue(1, 6);
+                            player2Randnum = randNumBattle;
+                            sprintf(player2Randstr, "%d", randNumBattle);
+                        }
+                    }else if(IsClicked(mouseOn3) == 2) battleBtnRec.y = rollFrameHeight;
+                    if(clickBattle == 0){
+                        *player1Randstr = "?";
+                        *player2Randstr = "?";
+                    }
+                    DrawText(player1Randstr, 100, 20, 50, BLACK);
+                    DrawText(player2Randstr, 200, 20, 50, BLACK);
+                    if(clickBattle == 1){
+                            battleBtnRec.y = 0;
+                            DrawTextureRec(battleBtn, battleBtnRec, (Vector2){battleBtnBound.x, battleBtnBound.y}, WHITE);
+                            if(player1Randnum > player2Randnum){
+                                playerPos[playerTurn] -= 1;
+                                clickBattle = 0;
+                                battleBegin = false;
+                                battleDone = true;
+                            }else if(player2Randnum > player1Randnum){
+                                playerPos[samePos] -= 1;
+                                clickBattle = 0;
+                                battleBegin = false;
+                                battleDone = true;
+                            }else{
+                                clickBattle = 0;
+                                player1Randnum = 0;
+                                player2Randnum = 0;
+                                *player1Randstr = "?";
+                                *player2Randstr = "?";
+                            }
+                            delayWin = true;
+                    }
+                }else{
+                    DrawTextureRec(rollBtn, rollBtnRec, (Vector2){rollBtnBound.x, rollBtnBound.y}, WHITE);
+                    if(IsClicked(mouseOn2) == 1){
+                        rollBtnRec.y = 0;
+                        randNum = GetRandomValue(1, 6);
+                        playerPos[playerTurn] = playerPos[playerTurn] + randNum;
+                        sprintf(randStr, "%d", randNum);
+                        playerRoll = true;
+                    }else if(IsClicked(mouseOn2) == 2) rollBtnRec.y = rollFrameHeight;
+                }
+                
+                DrawText(randStr, 1130, 500, 50, BLACK);
                 if(playerPos[playerTurn] > 39) playerPos[playerTurn] = 39 - (playerPos[playerTurn] % 39);
 
-                DrawRectanglePro(Char1, (Vector2){boardPosX[playerPos[playerTurn]], boardPosY[playerPos[playerTurn]]}, 0, BLACK);
+                DrawRectanglePro(Char1, (Vector2){boardPosX[playerPos[0]], boardPosY[playerPos[0]]}, 0, BLACK);
+                DrawRectanglePro(Char1, (Vector2){boardPosX[playerPos[1]], boardPosY[playerPos[1]]}, 0, RED);
+                DrawRectanglePro(Char1, (Vector2){boardPosX[playerPos[2]], boardPosY[playerPos[2]]}, 0, GREEN);
+                DrawRectanglePro(Char1, (Vector2){boardPosX[playerPos[3]], boardPosY[playerPos[3]]}, 0, BLUE);
+                
+                sprintf(playerPosStr[0], "%d", playerPos[0]);
+                sprintf(playerPosStr[1], "%d", playerPos[1]);
+                sprintf(playerPosStr[2], "%d", playerPos[2]);
+                sprintf(playerPosStr[3], "%d", playerPos[3]);
+            
+                DrawText(playerPosStr[0], 1030, 200, 40, BLACK);
+                DrawText(playerPosStr[1], 1030, 300, 40, RED);
+                DrawText(playerPosStr[2], 1030, 400, 40, GREEN);
+                DrawText(playerPosStr[3], 1030, 500, 40, BLUE);
+                
+                for(int i=0; i<=3; i++){
+                    if(playerPos[playerTurn] == playerPos[i] && i != playerTurn && playerPos[playerTurn] != 0){
+                        battleBegin = true;
+                        samePos = i;
+                        playerRoll = false;
+                        battleDone = false;
+                    }
+                }
+                
+                
+    
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
             }else{
                 DrawTexture(menuBg, 0, 0, WHITE);
                 DrawTextureRec(startBtn, startBtnRec, (Vector2){startBtnBound.x, startBtnBound.y}, WHITE);
@@ -119,7 +238,13 @@ int main(void)
                 }
             }
         EndDrawing();
-        
+        if(delayWin) delay(1);
+        delayWin = false;
+        if(playerRoll == true || battleDone == true){
+            playerTurn += 1;
+            playerRoll = false;
+            battleDone = false;
+        }
     }
     
     UnloadImage(btnImg);
